f1cd32d249fefa92176935a9c4c1689b958e41d1
Rubicker <newsl00001@gmail.com>
2019-07-23 16:56:57 +0800

    Utils / make diff work for attrs and children

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import createElement from './utils/createElement';
 import render from './utils/render';
 import mount from './utils/mount';
+import diff from './utils/diff';
 
 const createVApp = count =>
   createElement('div', {
@@ -9,26 +10,29 @@ const createVApp = count =>
       dataCount: count
     },
     children: [
-      createElement('input'),
       'Current count is: ',
       String(count),
-      createElement('img', {
-        attrs: {
-          src: 'https://media.giphy.com/media/tZYa9KrKcen1YUI6om/giphy.gif'
-        }
-      })
+      ...Array.from({ length: count }, () =>
+        createElement('img', {
+          attrs: {
+            src: 'https://media.giphy.com/media/tZYa9KrKcen1YUI6om/giphy.gif'
+          }
+        })
+      )
     ]
   });
 
-let count = 0;
-
-const vApp = createVApp(count);
+let vApp = createVApp(0);
 
 const $app = render(vApp);
 
 let $rootEl = mount($app, document.getElementById('app'));
 
 setInterval(() => {
-  count++;
-  $rootEl = mount(render(createVApp(count)), $rootEl);
+  const n = Math.floor(Math.random() * 10);
+  const vNewApp = createVApp(n);
+  const patch = diff(vApp, vNewApp);
+
+  $rootEl = patch($rootEl);
+  vApp = vNewApp;
 }, 1000);
